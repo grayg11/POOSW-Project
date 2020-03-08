360ab793d16d5149f0bf549d958ae8cbe535134a
Jody Kocis <jodykocis@Jodys-MacBook-Pro.local>
2020-03-08 16:45:31 -0400

    Updated DataMangerTests to Include objectivesCreated and enemiesStarted

--- SWIA/Assets/Tests/Editor/DataManagerTests.cs
+++ SWIA/Assets/Tests/Editor/DataManagerTests.cs
@@ -60,49 +60,8 @@ public class DataManagerTests : MonoBehaviour
     {
         var test = new GameObject().AddComponent<DataManager>();
 
-        var enemies = new List<GameObject>();
-        int amount = test.startingEnemies();
-        List<int> prevSpawns = new List<int>();
-        for (int i = 0; i < amount; i++)
-        {
-            // add weights for tiered enemy spawns
-            int type = test.pickEnemy();
-            UnitType ut = test.enemyTypes[type];
-            bool newSpawn = false;
-            int sp = Random.Range(0, test.GSC.generator.enemySpawns.Count - 1);
-
-            while (!newSpawn)
-            {
-                if (!prevSpawns.Contains(sp))
-                {
-                    newSpawn = true;
-                    prevSpawns.Add(sp);
-                }
-                else
-                {
-                    sp = Random.Range(0, test.GSC.generator.enemySpawns.Count - 1);
-                }
-            }
-
-            Vector3 position = test.GSC.generator.enemySpawns[sp] + ut.tileVisualPrefab.transform.position;
-            GameObject go = Instantiate(ut.tileVisualPrefab, position, ut.tileVisualPrefab.transform.rotation);
-            go.GetComponent<Unit>().map = test.GSC.map;
-            go.GetComponent<Unit>().unit = 6;
-            go.GetComponent<Unit>().tileX = (int)position.x;
-            go.GetComponent<Unit>().tileY = (int)position.y;
-            go.GetComponent<Unit>().isMobile = ut.isMobile;
-            go.GetComponent<Unit>().maxHealth = go.GetComponent<Unit>().health = ut.health;
-            go.GetComponent<Unit>().maxEndurance = go.GetComponent<Unit>().endurance = ut.endurance;
-            go.GetComponent<Unit>().movement = go.GetComponent<Unit>().MaxMovemment = ut.movement;
-            go.GetComponent<Unit>().defDice = ut.defDice;
-            go.GetComponent<Unit>().playerImage = ut.playerImage;
-            go.GetComponent<Unit>().playerCard = ut.playerCard;
-            go.GetComponent<Unit>().weapon = ut.weapon;
-            go.name = ut.name;
-            enemies.Add(go);
-        }
-
-        Assert.AreEqual(enemies, test.GSC.enemies);
+        test.createEnemies();
+        Assert.AreNotEqual(test.GSC.enemies.Count, 0);
     }
 
     [Test]
@@ -110,51 +69,37 @@ public class DataManagerTests : MonoBehaviour
     {
         var test = new GameObject().AddComponent<DataManager>();
 
-        List<Vector3> objectiveSpawns = test.GSC.generator.objectiveSpawns;
-        GameObject[] objectives = test.objectives;
-
-        int max = test.GSC.generator.enemySpawns.Count;
-        int val;
-
-        if (test.GSC.difficulty == 0)        // easy
-        {
-            val = Mathf.RoundToInt(Random.Range(.4f * max, .5f * max));
-        }
-        else if (test.GSC.difficulty == 1)   // normal
-        {
-            val = Mathf.RoundToInt(Random.Range(.25f * max, .4f * max));
-        }
-        else                            // hard
-        {
-            val = Mathf.RoundToInt(Random.Range(.1f * max, .3f * max));
-        }
-
-        if (val > max)
-            val = max;
-        for (int i = 0; i < val; i++)
-        {
-            GameObject randObj = objectives[Random.Range(0, objectives.Length)];
-            int sp = Random.Range(0, objectiveSpawns.Count - 1);
-            Vector3 position = objectiveSpawns[sp];
-            objectiveSpawns.RemoveAt(sp);
-            position += randObj.transform.position;
-            GameObject crateGo = Instantiate(randObj, position, randObj.transform.rotation);
-        }
-
         test.createObjectives();
-        Assert.AreEqual(objectiveSpawns, test.GSC.generator.objectiveSpawns);
-        Assert.AreEqual(objectives, test.objectives);
+        Assert.AreEqual(0, test.GSC.generator.objectiveSpawns.Count);
+        Assert.AreEqual(0, test.objectives.Length);
     }
 
     [Test]
     public void enemiesStarted()
     {
+        var test = new GameObject().AddComponent<DataManager>();
 
+        int max = test.GSC.generator.enemySpawns.Count;
+        int val = test.startingEnemies();
+
+        if (test.GSC.difficulty == 0) // easy
+        {
+            Assert.AreEqual((val == max || (val >= 2 && val <= 4)), true);
+        }
+        else if (test.GSC.difficulty == 1) // normal
+        {
+            Assert.AreEqual((val == max || (val >= 3 && val <= 5)), true);
+        }
+        else // hard
+        {
+            Assert.AreEqual((val == max || (val >= 4 && val <= 7)), true);
+        }
     }
 
     [Test]
     public void enemyPicked()
     {
+
 
     }
 }
