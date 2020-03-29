9c65db309f65362da99e23ba677607ade453b0f4
Jody Kocis <jodykocis@Jodys-MacBook-Pro.local>
2020-03-29 17:06:59 -0400

    Started GameUITests

---
+++ SWIA/Assets/Tests/Editor/GameUITests.cs
@@ -0,0 +1,103 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameUITests
+{
+    [Test]
+    public void HasStarted()
+    {
+
+    }
+
+    [Test]
+    public void HasUpdated()
+    {
+
+    }
+
+    [Test]
+    public void HasSelectedPlayer()
+    {
+
+    }
+
+    [Test]
+    public void HasMovedButton()
+    {
+
+    }
+
+    [Test]
+    public void HasConfirmedMove()
+    {
+
+    }
+
+    [Test]
+    public void HasRested()
+    {
+
+    }
+
+    [Test]
+    public void HasChangedRestText()
+    {
+
+    }
+
+    [Test]
+    public void AttackButtonAction()
+    {
+
+    }
+
+    [Test]
+    public void HasHealed()
+    {
+
+    }
+
+    [Test]
+    public void HasUpdated()
+    {
+
+    }
+
+    [Test]
+    public void HasNotHealed()
+    {
+
+    }
+
+    [Test]
+    public void HasRestHealed()
+    {
+
+    }
+
+    [Test]
+    public void HasNotMoved()
+    {
+
+    }
+
+    [Test]
+    public void HasEndedTurn()
+    {
+
+    }
+
+    [Test]
+    public void ActionButtonAction()
+    {
+
+    }
+
+    [Test]
+    public void HasFoundNextState()
+    {
+
+    }
+}
