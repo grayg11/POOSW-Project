38c1e3139dd78f7e439a6f1c01577ae359685ad1
Ipleau <ipleau@knights.ucf.edu>
2020-03-29 17:51:08 -0400

    InGameMenu
    Added an In game Menu

---
+++ SWIA/Assets/Pause_Menu.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Pause_Menu : MonoBehaviour
+{
+    public static bool Paused = false;
+    public GameObject pauseMenuUI;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //PausePanel.SetActive(false);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(Paused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        Paused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        Paused = true;
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene("Assets/Scenes/Main Menu.unity");
+    }
+
+    public void Quit()
+    {
+        if(UnityEditor.EditorApplication.isPlaying == true)
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+        else
+        {
+            Application.Quit();
+        }
+    }
+}
